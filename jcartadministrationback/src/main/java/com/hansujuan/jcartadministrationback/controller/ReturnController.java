d478084294f2125dc1e88b0c56f20c4a2aaa0530
hansujuan <573766829@qq.com>
2020-02-25 16:22:50 +0800

    add administrator return apis

---
+++ jcartadministrationback/src/main/java/com/hansujuan/jcartadministrationback/controller/ReturnController.java
@@ -0,0 +1,30 @@
+package com.hansujuan.jcartadministrationback.controller;
+
+import com.hansujuan.jcartadministrationback.dto.in.ReturnSearchInDTO;
+import com.hansujuan.jcartadministrationback.dto.in.ReturnUpdateActionInDTO;
+import com.hansujuan.jcartadministrationback.dto.out.PageOutDTO;
+import com.hansujuan.jcartadministrationback.dto.out.ReturnListOutDTO;
+import com.hansujuan.jcartadministrationback.dto.out.ReturnShowOutDTO;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/return")
+public class ReturnController {
+
+    @GetMapping("/search")
+    public PageOutDTO<ReturnListOutDTO> search(@RequestBody ReturnSearchInDTO returnSearchInDTO,
+                                               @RequestParam Integer pageNum){
+        return null;
+    }
+
+    @GetMapping("/getById")
+    public ReturnShowOutDTO getById(@RequestParam Integer returnId){
+        return null;
+    }
+
+    @PostMapping("/updateAction")
+    public void updateAction(@RequestBody ReturnUpdateActionInDTO returnUpdateActionInDTO){
+
+    }
+
+}
