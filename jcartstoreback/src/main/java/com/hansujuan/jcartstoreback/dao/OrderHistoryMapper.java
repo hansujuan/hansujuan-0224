ef8fa16baeeec56715656e0d0020babd22966b50
hansujuan <573766829@qq.com>
2020-02-26 14:57:31 +0800

    add store Mybatis files

---
+++ jcartstoreback/src/main/java/com/hansujuan/jcartstoreback/dao/OrderHistoryMapper.java
@@ -0,0 +1,17 @@
+package com.hansujuan.jcartstoreback.dao;
+
+import com.hansujuan.jcartstoreback.po.OrderHistory;
+
+public interface OrderHistoryMapper {
+    int deleteByPrimaryKey(Long orderHistoryId);
+
+    int insert(OrderHistory record);
+
+    int insertSelective(OrderHistory record);
+
+    OrderHistory selectByPrimaryKey(Long orderHistoryId);
+
+    int updateByPrimaryKeySelective(OrderHistory record);
+
+    int updateByPrimaryKey(OrderHistory record);
+}
