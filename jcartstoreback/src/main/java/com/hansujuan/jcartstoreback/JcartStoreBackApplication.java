87ee0ba5d94a99ef0447b8d18c9e5d42a12a90b2
hansujuan <573766829@qq.com>
2020-02-26 14:58:02 +0800

    add store mybatis files

--- jcartstoreback/src/main/java/com/hansujuan/jcartstoreback/JcartStoreBackApplication.java
+++ jcartstoreback/src/main/java/com/hansujuan/jcartstoreback/JcartStoreBackApplication.java
@@ -1,9 +1,11 @@
 package com.hansujuan.jcartstoreback;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.hansujuan.jcartstoreback.dao")
 public class JcartStoreBackApplication {
 
     public static void main(String[] args) {
