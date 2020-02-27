bc90fa32f3db9cf05da22879b5b543500fa5f748
hansujuan <573766829@qq.com>
2020-02-27 14:42:26 +0800

    add administrator image upload apis

---
+++ jcartadministrationback/src/main/java/com/hansujuan/jcartadministrationback/constant/ClientExceptionConstant.java
@@ -0,0 +1,6 @@
+package com.hansujuan.jcartadministrationback.constant;
+
+public class ClientExceptionConstant {
+    public static final String IMAGE_INVALID_ERRCODE = "0001";
+    public static final String IMAGE_INVALID_ERRMSG = "image is not jpg(jepg) or png";
+}
--- jcartadministrationback/src/main/java/com/hansujuan/jcartadministrationback/controller/ImageController.java
+++ jcartadministrationback/src/main/java/com/hansujuan/jcartadministrationback/controller/ImageController.java
@@ -1,15 +1,42 @@
 package com.hansujuan.jcartadministrationback.controller;
 
+import com.hansujuan.jcartadministrationback.constant.ClientExceptionConstant;
+import com.hansujuan.jcartadministrationback.exception.ClientException;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+
 @RestController
 @RequestMapping("/image")
+@CrossOrigin
 public class ImageController {
 
+    private List<String> imageExts= Arrays.asList("jpg","jpeg","png");
+
     @PostMapping("/upload")
-    public String upload(@RequestParam MultipartFile image){
-        return null;
+    public String upload(@RequestParam MultipartFile image) throws IOException, ClientException {
+        String originalFilename = image.getOriginalFilename();
+        String[] split = originalFilename.split("\\.");
+        String ext = split[split.length-1];
+        ext = ext.toLowerCase();
+        boolean contains = imageExts.contains(ext);
+        if(!contains){
+            throw new ClientException(ClientExceptionConstant.IMAGE_INVALID_ERRCODE,ClientExceptionConstant.IMAGE_INVALID_ERRMSG);
+        }
+        String uuid = UUID.randomUUID().toString();
+        String filename = String.format("%s.%s", uuid, ext);
+        String filepath = String.format("www/image/%s", filename);
+        try(FileOutputStream out = new FileOutputStream(filepath)){
+            byte[] data = image.getBytes();
+            out.write(data);
+        }
+        return filename;
     }
 
 }
