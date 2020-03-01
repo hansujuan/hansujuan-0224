0d50850e6d4b3d02ce96545148cef5e84dc28fba
hansujuan <573766829@qq.com>
2020-03-01 16:41:20 +0800

    add admin and store filter

--- jcartadministrationback/src/main/java/com/hansujuan/jcartadministrationback/constant/ClientExceptionConstant.java
+++ jcartadministrationback/src/main/java/com/hansujuan/jcartadministrationback/constant/ClientExceptionConstant.java
@@ -1,9 +1,9 @@
 package com.hansujuan.jcartadministrationback.constant;
 
 public class ClientExceptionConstant {
-//    public static final String IMAGE_INVALID_ERRCODE = "0001";
-//    public static final String IMAGE_INVALID_ERRMSG = "image is not jpg(jepg) or png";
 
+    public static final String NOT_SUPPORT_STATIC_RESOURCE_ERRCODE = "0001";
+    public static final String NOT_SUPPORT_STATIC_RESOURCE_ERRMSG = "not support static resource";
     public static final String TOKEN_NOT_EXIST_ERRCODE = "0002";
     public static final String TOKEN_NOT_EXIST_ERRMSG = "token not exist";
     public static final String TOKEN_INVALID_ERRCODE = "0003";
@@ -15,4 +15,6 @@ public class ClientExceptionConstant {
     public static final String ADMINISTRATOR_USERNAME_NOT_EXIST_ERRMSG = "administrator username not exist";
     public static final String ADNINISTRATOR_PASSWORD_INVALID_ERRCODE = "0102";
     public static final String ADNINISTRATOR_PASSWORD_INVALID_ERRMSG = "administrator password invalid";
+
+
 }
