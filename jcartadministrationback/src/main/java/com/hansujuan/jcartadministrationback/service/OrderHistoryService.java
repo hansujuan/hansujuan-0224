59e4429d97b5db5b96b7519b9fa91826e45763f2
hansujuan <573766829@qq.com>
2020-03-05 16:55:31 +0800

    add administrator orderhistory apis

--- jcartadministrationback/src/main/java/com/hansujuan/jcartadministrationback/service/OrderHistoryService.java
+++ jcartadministrationback/src/main/java/com/hansujuan/jcartadministrationback/service/OrderHistoryService.java
@@ -1,4 +1,11 @@
 package com.hansujuan.jcartadministrationback.service;
 
+import com.hansujuan.jcartadministrationback.po.OrderHistory;
+
+import java.util.List;
+
 public interface OrderHistoryService {
+    List<OrderHistory> getByOrderId(Long orderId);
+
+    Long create(OrderHistory orderHistory);
 }
--- jcartadministrationback/src/main/java/com/hansujuan/jcartadministrationback/service/impl/OrderHistoryServiceImpl.java
+++ jcartadministrationback/src/main/java/com/hansujuan/jcartadministrationback/service/impl/OrderHistoryServiceImpl.java
@@ -1,10 +1,27 @@
 package com.hansujuan.jcartadministrationback.service.impl;
 
+import com.hansujuan.jcartadministrationback.dao.OrderHistoryMapper;
+import com.hansujuan.jcartadministrationback.po.OrderHistory;
 import com.hansujuan.jcartadministrationback.service.OrderHistoryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 public class OrderHistoryServiceImpl implements OrderHistoryService {
 
+    @Autowired
+    private OrderHistoryMapper orderHistoryMapper;
 
+    public List<OrderHistory> getByOrderId(Long orderId) {
+        return orderHistoryMapper.selectByOrderId(orderId);
+    }
+
+    @Transactional
+    public Long create(OrderHistory orderHistory) {
+        orderHistoryMapper.insertSelective(orderHistory);
+        return orderHistory.getOrderHistoryId();
+    }
 }
