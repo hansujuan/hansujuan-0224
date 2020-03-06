2484dd68d08740f33450694381f4afdf7893c88a
hansujuan <573766829@qq.com>
2020-03-06 16:15:45 +0800

    add administrator front order-search

--- jcart-administration-front/viewmodels/order-search.js
+++ jcart-administration-front/viewmodels/order-search.js
@@ -1,4 +1,32 @@
 var app = new Vue({
     el: '#app',
-    data: {}
+    data: {
+        pageInfo:'',
+        pageNum:1
+    },
+    mounted(){
+        console.log('order search');
+        this.orderSearchList();
+    },
+    methods:{
+        handlePageChange(val){
+            console.log('pageNum change');
+            this.pageNum=val;
+            this.orderSearchList();
+        },
+        orderSearchList(){
+            axios.get('/order/search', {
+                params: {
+                    pageNum: this.pageNum
+                }
+            })
+                .then(function (response) {
+                    console.log(response);
+                    app.pageInfo = response.data;
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+        }
+    }
 })
