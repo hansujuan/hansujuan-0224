b4f049ae87c937120430a9ed64a6182f43915909
hansujuan <573766829@qq.com>
2020-03-08 10:09:07 +0800

    add store front return index and show

--- jcart-store-front/viewmodels/return-index.js
+++ jcart-store-front/viewmodels/return-index.js
@@ -1,4 +1,32 @@
 var app = new Vue({
     el: '#app',
-    data: {}
+    data: {
+        pageInfo: '',
+        pageNum: 1
+    },
+    mounted() {
+        console.log('return list');
+        this.getMyReturn();
+    },
+    methods: {
+        handlePageChange(val){
+            console.log('pageNum change');
+            this.pageNum=val;
+
+        },
+        getMyReturn() {
+            axios.get('/return/getList', {
+                params: {
+                    pageNum: this.pageNum
+                }
+            })
+                .then(function (response) {
+                    console.log(response);
+                    app.pageInfo = response.data;
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+        }
+    }
 })
