0030fc84260a5e2a205ffbd7b08fb28d8cd03883
hansujuan <573766829@qq.com>
2020-03-10 15:40:54 +0800

    add administrator back return vague search

--- jcartadministrationback/src/main/java/com/hansujuan/jcartadministrationback/controller/ReturnController.java
+++ jcartadministrationback/src/main/java/com/hansujuan/jcartadministrationback/controller/ReturnController.java
@@ -26,7 +26,7 @@ public class ReturnController {
     @GetMapping("/search")
     public PageOutDTO<ReturnListOutDTO> search(ReturnSearchInDTO returnSearchInDTO
             ,@RequestParam(required = false, defaultValue = "1") Integer pageNum){
-        Page<Return> search = returnService.search(pageNum);
+        Page<Return> search = returnService.search(returnSearchInDTO,pageNum);
         List<ReturnListOutDTO> collect = search.stream().map(aReturn -> {
             ReturnListOutDTO returnListOutDTO = new ReturnListOutDTO();
             returnListOutDTO.setReturnId(aReturn.getReturnId());
--- jcartadministrationback/src/main/java/com/hansujuan/jcartadministrationback/service/ReturnService.java
+++ jcartadministrationback/src/main/java/com/hansujuan/jcartadministrationback/service/ReturnService.java
@@ -1,11 +1,12 @@
 package com.hansujuan.jcartadministrationback.service;
 
 import com.github.pagehelper.Page;
+import com.hansujuan.jcartadministrationback.dto.in.ReturnSearchInDTO;
 import com.hansujuan.jcartadministrationback.po.Return;
 
 public interface ReturnService {
 
-    Page<Return> search(Integer pageNum);
+    Page<Return> search(ReturnSearchInDTO returnSearchInDTO,Integer pageNum);
 
     Return getById(Integer returnId);
 
