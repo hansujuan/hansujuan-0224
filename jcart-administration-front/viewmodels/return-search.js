915c3bbd318138426fc0898d613a9e546cb8b4b2
hansujuan <573766829@qq.com>
2020-03-11 15:05:32 +0800

    add administrator front return filter query

--- jcart-administration-front/viewmodels/return-search.js
+++ jcart-administration-front/viewmodels/return-search.js
@@ -2,13 +2,43 @@ var app = new Vue({
     el: '#app',
     data: {
         pageInfo:'',
-        pageNum:1
+        pageNum:1,
+        returnId: '',
+        orderId: '',
+        customerName: '',
+        productCode: '',
+        productName: '',
+        selectedStatus: '',
+        statuses: [
+            { value: 0, label: '待处理' },
+            { value: 1, label: '待取货' },
+            { value: 2, label: '正在处理' },
+            { value: 3, label: '完成' },
+            { value: 4, label: '拒绝' }
+        ],
+        startTime: '',
+        endTime: '',
     },
     mounted(){
         console.log('mounted view');
         this.searchReturn();
     },
     methods:{
+        handleSearchClick(){
+            this.pageNum=1;
+            this.searchReturn();
+        },
+        handleClearClick(){
+            this.returnId= '';
+            this.orderId='';
+            this.customerName='';
+            this.productCode='';
+            this.productName='';
+            this.selectedStatus='';
+            this.startTime='';
+            this.endTime='';
+            this.searchReturn();
+        },
         handlePageChange(val){
             console.log('pageNum change');
             this.pageNum=val;
@@ -17,7 +47,15 @@ var app = new Vue({
         searchReturn(){
             axios.get('/return/search', {
                 params: {
-                    pageNum: this.pageNum
+                    pageNum: this.pageNum,
+                    returnId: this.returnId,
+                    orderId: this.orderId,
+                    customerName: this.customerName,
+                    productCode: this.productCode,
+                    productName: this.productName,
+                    status: this.selectedStatus,
+                    startTimestamp: this.startTime ? this.startTime.getTime() : '',
+                    endTimestamp: this.endTime ? this.endTime.getTime() : '',
                 }
             })
                 .then(function (response) {
