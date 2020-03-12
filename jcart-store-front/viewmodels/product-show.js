7ea8215e0129b8622ff9df135ea903eb0dd76fc4
hansujuan <573766829@qq.com>
2020-03-12 16:19:09 +0800

    fixed store order index

--- jcart-store-front/viewmodels/product-show.js
+++ jcart-store-front/viewmodels/product-show.js
@@ -49,6 +49,7 @@ var app = new Vue({
                     productName: this.productName,
                     mainPicUrl: this.mainPicUrl,
                     unitPrice: this.price,
+                    discount:this.discount,
                     quantity: this.quantity
                 };
                 this.myShoppingCart.push(cartProduct);
--- jcart-store-front/viewmodels/shopping-cart.js
+++ jcart-store-front/viewmodels/shopping-cart.js
@@ -2,14 +2,32 @@ var app = new Vue({
     el: '#app',
     data: {
         myShoppingCart: []
+
+    },
+    computed:{
+        totalPrice(){
+            var subTotalPrices=this.myShoppingCart.map(p=>{
+                return p.unitPrice * p.discount * p.quantity;
+            });
+            var totalPrice = subTotalPrices.reduce((a, b) => a + b, 0);
+            var totalPriceStr = totalPrice.toFixed(2);
+            totalPrice = parseFloat(totalPriceStr);
+            return totalPrice;
+        }
     },
     mounted(){
+        
         console.log('mounted shuchu');
         var myShoppingCartJson = localStorage['myShoppingCartJson'];
         // console.log(myShoppingCartJson);
         this.myShoppingCart = myShoppingCartJson ? JSON.parse(myShoppingCartJson) : [];
     },
     methods:{
+        handleClearCart(){
+            console.log('clear cart click');
+            this.myShoppingCart = [];
+            localStorage.removeItem('myShoppingCartJson');
+        },
         handleDelete(index, row){
             console.log('delete');
             this.myShoppingCart.splice(index, 1);
