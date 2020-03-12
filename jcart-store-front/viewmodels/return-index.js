10997887e2daca3ba0d883f4f0e7adff05c0024e
hansujuan <573766829@qq.com>
2020-03-12 17:15:32 +0800

    fixed store return index

--- jcart-store-front/viewmodels/return-index.js
+++ jcart-store-front/viewmodels/return-index.js
@@ -5,19 +5,11 @@ var app = new Vue({
         pageNum: 1,
         statuses: [
             { value: 0, label: '待处理' },
-            { value: 1, label: '处理中' },
-            { value: 2, label: '待发货' },
-            { value: 3, label: '已发货' },
-            { value: 4, label: '待签收' },
-            { value: 5, label: '已签收' },
-            { value: 6, label: '待支付' },
-            { value: 7, label: '已支付' },
-            { value: 8, label: '取消' },
-            { value: 9, label: '拒绝' },
-            { value: 10, label: '完成' },
-            { value: 11, label: '待评价' },
-            { value: 12, label: '已评价' }
-        ],
+            { value: 1, label: '待取货' },
+            { value: 2, label: '正在处理' },
+            { value: 3, label: '完成' },
+            { value: 4, label: '拒绝' }
+        ]
         
     },
     mounted() {
